87e2273c6090c84824d3cea2cc8a5ae9a4a66ba9
Anup Bharadwaj <bharadwajanup91@gmail.com>
2017-04-14 00:44:37 -0400

    Add PacketDownloader

---
+++ MPTCPClient/src/main/java/PacketDownloader.java
@@ -0,0 +1,48 @@
+import Network.NetworkPacket;
+
+import java.io.*;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created as part of the class project for Mobile Computing
+ */
+public class PacketDownloader implements Runnable {
+
+    private String path;
+    private BlockingQueue<NetworkPacket> packets;
+
+    public PacketDownloader(BlockingQueue<NetworkPacket> queue, String path) {
+        this.packets = queue;
+        this.path = path;
+
+
+    }
+
+    @Override
+    public void run() {
+        FileOutputStream fout = null;
+        RandomAccessFile arrayFile = null;
+        BufferedOutputStream bos = null;
+        try {
+            fout = new FileOutputStream(path);
+            arrayFile = new RandomAccessFile(path, "rw");
+            bos = new BufferedOutputStream(fout);
+            while (true) {
+                NetworkPacket packet = packets.take();
+                arrayFile.seek(packet.getId() - 1);
+                bos.write(packet.getData(), 0, packet.getLength());
+                System.out.println(String.format("Worker FileDownloader stored the packet %d", packet.getId()));
+            }
+        } catch (FileNotFoundException fex) {
+            System.out.println("File Not Found");
+            fex.printStackTrace();
+
+        } catch (InterruptedException e) {
+            System.out.println("Thread was Interrupted");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("There was an IO exception");
+            e.printStackTrace();
+        }
+    }
+}
